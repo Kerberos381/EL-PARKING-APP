99ac33de5ce6648e96f08898fcae91b6e18859e9
Stiv <stiv.malakjan@gmail.com>
2026-03-26 12:57:34 +0100

    Initial Commit

---
+++ EL PARKING APP/EL_PARKING_APPApp.swift
@@ -0,0 +1,17 @@
+//
+//  EL_PARKING_APPApp.swift
+//  EL PARKING APP
+//
+//  Created by Stiv Malakjan on 26.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct EL_PARKING_APPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EL PARKING APPTests/EL_PARKING_APPTests.swift
@@ -0,0 +1,16 @@
+//
+//  EL_PARKING_APPTests.swift
+//  EL PARKING APPTests
+//
+//  Created by Stiv Malakjan on 26.03.2026.
+//
+
+import Testing
+
+struct EL_PARKING_APPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

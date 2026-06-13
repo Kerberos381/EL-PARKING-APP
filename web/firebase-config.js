87781eb1d717081d92ca2b1905c740cd081fc419
Kerberos381 <stiv.malakjan@gmail.com>
2026-06-13 11:45:17 +0200

    Add Firebase configuration file

---
+++ web/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD1XYo3kV4vGKhOvFJrndNMrL2LhEuIg1o",
+  authDomain: "el-parking-app.firebaseapp.com",
+  projectId: "el-parking-app",
+  storageBucket: "el-parking-app.firebasestorage.app",
+  messagingSenderId: "58986005782",
+  appId: "1:58986005782:web:a887ca3482851c5b0a3c37",
+};
